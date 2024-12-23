92413797b4cb59845a637aab2f0356d30be7203c
MohammedSamerr <mohamed.samir2413@gmail.com>
2024-12-23 15:32:11 +0200

    V2.0

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-grid.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+
+  products:Product[]=[];
+  currentCategoryId: number=0 ;
+  
+  constructor(private productService:ProductService,private route: ActivatedRoute){}
+
+  ngOnInit():void{
+    this.route.paramMap.subscribe(()=>{
+      this.listProducts();
+    });
+    
+  }
+
+  listProducts(){
+    // check if "id" pramter is available
+    const isExistId : boolean = this.route.snapshot.paramMap.has('id');
+
+    if(isExistId){
+      this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+    }else{
+      //default to Category Id = 1
+      this.currentCategoryId=1;
+    }
+
+
+    this.productService.getProductList(this.currentCategoryId).subscribe(
+      data=>{
+        this.products =data;
+      }
+    )
+  }
+
+}
